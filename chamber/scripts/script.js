bb4092847be38a76c146ca60c5123a0e490048ae
sowbyspencer@gmail.com <33128124+spencer-sowby-owatc@users.noreply.github.com>
2022-10-13 18:36:47 -0600

    More Chamber updates

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,23 +1,43 @@
 const d = new Date();
 const year = d.getFullYear();
+const day = d.getDay();
 
-const yearel = document.querySelector("#year")
-yearel.innerHTML = year
+const yearel = document.querySelector("#year");
+yearel.innerHTML = year;
 
-const timestamp = document.querySelector("#timestamp")
-timestamp.innerHTML = document.lastModified
+const timestamp = document.querySelector("#timestamp");
+timestamp.innerHTML = document.lastModified;
 
 
 
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(d);
 
-const datestamp =document.querySelector("#date")
-datestamp.innerHTML = fulldateUK
+const datestamp = document.querySelector("#date");
+datestamp.innerHTML = fulldateUK;
 
 function toggleMenu() {
-    document.querySelector("#primary-nav").classList.toggle("open")
-    document.querySelector("#hamburger-btn").classList.toggle("open")
+    document.querySelector("#primary-nav").classList.toggle("open");
+    document.querySelector("#hamburger-btn").classList.toggle("open");
 }
 
-const hBtn = document.querySelector("#hamburger-btn")
+const hBtn = document.querySelector("#hamburger-btn");
 hBtn.onclick = toggleMenu;
+
+function toggleBanner() {
+    banner.classList.toggle("display");
+}
+
+const banner = document.querySelector("#banner");
+const bannerBtn = document.getElementById("bannerBtn")
+
+bannerBtn.addEventListener("click", function(){
+    toggleBanner();
+})
+
+// Display banner if it s Monday or Tuesday
+if (day === 1 || day === 2){
+    toggleBanner();
+    console.log(day);
+}
+
+
