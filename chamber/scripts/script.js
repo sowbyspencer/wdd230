1c7c26e8be1fcc9971059ca5e774adae440d9538
sowbyspencer@gmail.com <33128124+spencer-sowby-owatc@users.noreply.github.com>
2022-10-29 20:30:45 -0600

    Discover Page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -45,3 +45,59 @@ if (day === 1 || day === 2){
 }
 
 
+
+
+
+
+
+
+// A list of images with the data-src attribute.
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// Function to change html for images
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {image.removeAttribute("data-src");};
+};
+
+// IntersectionObserver options
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
+}
+
+// The Intersection Observer controls images showing when in the viewport
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {observer.observe(img);});
+}
+// If the browser doesn't support Intersection Observer load all the images
+else {
+    imagesToLoad.forEach((img) => {loadImages(img);});
+}
+
+
+
+
+
+
+const userVisitEl = document.querySelector("#user-visit")
+
+let lastVisit = Number(window.localStorage.getItem("last-visit"))
+
+if (lastVisit !== 0){
+    let diff = (Date.now() - lastVisit) / (1000*60*60*24)
+    userVisitEl.textContent = `It has been: ` + Math.round(diff) + ` Days since you last visited.`
+} else{
+    userVisitEl.textContent = `This is your first visit.`
+}
+
+localStorage.setItem("last-visit", Date.now())
