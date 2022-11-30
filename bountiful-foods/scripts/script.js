f3b957f5e8e470278e212784e53c733249d59840
sowbyspencer@gmail.com <33128124+spencer-sowby-owatc@users.noreply.github.com>
2022-11-29 22:33:30 -0700

    Bountiful Foods

---
+++ bountiful-foods/scripts/script.js
@@ -0,0 +1,5 @@
+
+/* Getting the timestamp from the document and putting it in the footer. */
+const timestamp = document.querySelector("#timestamp");
+timestamp.innerHTML = document.lastModified;
+
