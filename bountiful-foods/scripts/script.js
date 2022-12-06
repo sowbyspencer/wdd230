3e73efda84aca9520be1a70e4637e6639219ff5e
sowbyspencer@gmail.com <33128124+spencer-sowby-owatc@users.noreply.github.com>
2022-12-06 03:36:15 -0700

    Some

--- bountiful-foods/scripts/script.js
+++ bountiful-foods/scripts/script.js
@@ -3,3 +3,18 @@
 const timestamp = document.querySelector("#timestamp");
 timestamp.innerHTML = document.lastModified;
 
+
+
+/**
+ * When the user clicks the hamburger button, toggle the open class on the primary-nav and
+ * hamburger-btn elements.
+ */
+ function toggleMenu() {
+    document.querySelector("#primary-nav").classList.toggle("open");
+    document.querySelector("#hamburger-btn").classList.toggle("open");
+}
+
+/* This is adding an event listener to the hamburger button. When the button is clicked it will run the
+toggleMenu function. */
+const hBtn = document.querySelector("#hamburger-btn");
+hBtn.onclick = toggleMenu;
